dc20cc4e666f978a4434de7e66428f0065522d98
Shiv kumar <119939586+shivuser75@users.noreply.github.com>
2022-12-07 16:32:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,17 +8,17 @@ class Development(Config):
     # the name to display in your alive message
     ALIVE_NAME = "rishabh"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://hwtxqqfc:U_QHjE6TByrb8MVJOuEcfDod9FXa1BkN@satao.db.elephantsql.com/hwtxqqfc"
+    DB_URI = "postgres://jrycfssc:fEZMYWiOcO9vB8cIq1_Z97GhW3TiTppJ@tiny.db.elephantsql.com/jrycfssc"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "1BJWap1wBuyvjx2Gpd9jDpJRQXbAw8X-JKKWYvOcPWqHAgOgNuIQ7n1aICSLmxaFuN1kqUEMwzfByRhUqdnTZ_J0MWrc2_6J7sErEjuHpN3AWmmr4ODJwyAl8lGRHmgVeIh8kjr2Gv5jMVQ7AIC6jqzbYXcWZho8dGPf2bwl0jpkmF0bQ9qNZcJwLb4cEs-Z7hHUbQN29pk4FRDpfJDY1VHudKGkv1Ta_vEI8aekatvyBEkO2c8Jqnuxw_hHGTIKuln92d0AFQmjLtI3-nT2SkdUCkl9n_RoUTF9Vt67Q2Wy8o9444TM-Y9PF7UK3HI71pHpvAwZiZqDVGyMDHYeRQ28RLb8n9A0="
+    STRING_SESSION = "1BVtsOIQBuz07scRG6htMQaNCyuj3cs6R2TA1sp6x415xJKlNa2ONPkMa3vChdkmNOqU8t_Fkw1tY5W4wCacQLcAjt64VT-7g2BADy0mqaOthQyog8c0kqDDRx5XyWa3hNTlQf6dT2jiDWJ4oOzHNcJZYJ1r2b-lkaF0XeeQbjcXtczdbU_0O78xhnaQD1qn4pp1AQwTdVlfM-CbLIjNEspm5jXhCQnCZVXKc49NuzLK4NEcTFdcnKmqAbPydzjauN6iBs1NtSiuECLVL1lESQ4WxA1cjmFB6ljH_ElR_79mDRLS4VZ93OLHJrEG9PqNUPm6bqix1F9Jeug2gFnzsPGhzPSK1lhk="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "5934016434:AAFPcuDwT0TqB2dUixuLBvtkFPKXRvcqnUc"
+    TG_BOT_TOKEN = "5983600849:AAFJMUaeNx2S0dIqklo3HjNvermZNhUiLFM"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -1001837587034
+    PRIVATE_GROUP_BOT_API_ID = -1001762139427
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
-    VC_SESSION = "1BJWap1wBuzviE9Ue-g3Rfcsun0XFLYD3FVxUZJvvTAJh4izRZlv5-ToyFzWvdtC2IRvJgAVMY-9xxP7nrPv6GpPgZ8ma2g148gZs3EGAnb0Cdls1D4OVXBhV9yu79vV6y0tSpZuudBlRFmRuQiJUxS9WmHGzX76GebKe1xUEtM4n3Lwd0NoA0x0cZ4QA8Nt25ZzJazvUxX2O15MXVakl3hT4kTA1CLr6DJIGp0T6iRlehy5FvWI0XmlavX1bW6tJxfgGeiJkS7aB_unD0-airBE5ohJ149-VW9E5aX95YXTlt2ZL1GokuUFHfxsYXDqxd3det2IU7ggi1iG1QV1T6d-9W2hOcDU="
+    VC_SESSION = "1BVtsOIQBu3mJL9Wx6W2eRMl-FqKWHbUNmFIqhuYYE-DkJLvN98dz9aCT_pu8Uqw5gBaXylsjIaRRdnFVVtXqsR6O4r0jZlhFkFId_EExVrRJuKJWGpjc5mstXGc7nG4CQNAUQrTP-nBGoDBKpRGVWeeR2GKHNIgRc6JaIWL0IfgbkyC21X4wjtH1p-6-ZWGtv7fll_z6maSrKjlfgAe1P94bigDjvNBcpip12GELD21Ft1AxQ3d_bvyHz_KqESD-ja9kpJd-W_9l3EwpPDmk68z8MsruHYf75yYEaDRaaAFYLuSnqLF0Y6ad9nW9EuG1jfrPBtYbH0FefyHOHcAFNgen0gqpoik="
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
