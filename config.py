19a540a0b6e9bfabaecbfb18d597711c27fb1d2f
Shiv kumar <119939586+shivuser75@users.noreply.github.com>
2022-12-07 08:07:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,15 +10,15 @@ class Development(Config):
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://hwtxqqfc:U_QHjE6TByrb8MVJOuEcfDod9FXa1BkN@satao.db.elephantsql.com/hwtxqqfc"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "1BVtsOGgBu21dLz241cIdffngb3vF5b400tG2ARIv9j48fNjbCn_HLblpx8UeAyzUNsR7RPjx3z_QNbDHfGgsyRYQe7EMwJ8HhHUGbjJuVbHAomifZuIFoPFEYbngy0pHRgyMQylOg4iYIInczI0onBnoeK-7PfbD0mLU3EHrV8D87UgbgrK7jKpajOPJ0sM5RfG5C1AVP__6UErvqy65B23Z-CS5wWRiuMpvMEI9-bsalbqeMleorC49tycwOC0Ab1e5WOteqKS-S0grhBWFXubc6QNYDLW_3VxJFWOkAU6sxG9IlMoBH2KQ8ERKeaeLxOCKK0VkqyFnvTyWSMSbWCrClwvZM4M="
+    STRING_SESSION = "1BJWap1wBuyvjx2Gpd9jDpJRQXbAw8X-JKKWYvOcPWqHAgOgNuIQ7n1aICSLmxaFuN1kqUEMwzfByRhUqdnTZ_J0MWrc2_6J7sErEjuHpN3AWmmr4ODJwyAl8lGRHmgVeIh8kjr2Gv5jMVQ7AIC6jqzbYXcWZho8dGPf2bwl0jpkmF0bQ9qNZcJwLb4cEs-Z7hHUbQN29pk4FRDpfJDY1VHudKGkv1Ta_vEI8aekatvyBEkO2c8Jqnuxw_hHGTIKuln92d0AFQmjLtI3-nT2SkdUCkl9n_RoUTF9Vt67Q2Wy8o9444TM-Y9PF7UK3HI71pHpvAwZiZqDVGyMDHYeRQ28RLb8n9A0="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "5509473272:AAEXFx2yPYrY_KvL4B9lrn3Zh0jzhJWPaBs"
+    TG_BOT_TOKEN = "5934016434:AAFPcuDwT0TqB2dUixuLBvtkFPKXRvcqnUc"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -1001623978356
+    PRIVATE_GROUP_BOT_API_ID = -1001837587034
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
-    VC_SESSION = "your 2nd account telethon session"
+    VC_SESSION = "1BJWap1wBuzviE9Ue-g3Rfcsun0XFLYD3FVxUZJvvTAJh4izRZlv5-ToyFzWvdtC2IRvJgAVMY-9xxP7nrPv6GpPgZ8ma2g148gZs3EGAnb0Cdls1D4OVXBhV9yu79vV6y0tSpZuudBlRFmRuQiJUxS9WmHGzX76GebKe1xUEtM4n3Lwd0NoA0x0cZ4QA8Nt25ZzJazvUxX2O15MXVakl3hT4kTA1CLr6DJIGp0T6iRlehy5FvWI0XmlavX1bW6tJxfgGeiJkS7aB_unD0-airBE5ohJ149-VW9E5aX95YXTlt2ZL1GokuUFHfxsYXDqxd3det2IU7ggi1iG1QV1T6d-9W2hOcDU="
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
