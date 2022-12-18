214e9c4a2ad7ca33d4925d8a48cf934988847933
RISHABH ANAND <119417555+thanosusers@users.noreply.github.com>
2022-12-18 15:18:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,18 +10,18 @@ class Development(Config):
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://jrycfssc:fEZMYWiOcO9vB8cIq1_Z97GhW3TiTppJ@tiny.db.elephantsql.com/jrycfssc"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "1BVtsOIQBuz07scRG6htMQaNCyuj3cs6R2TA1sp6x415xJKlNa2ONPkMa3vChdkmNOqU8t_Fkw1tY5W4wCacQLcAjt64VT-7g2BADy0mqaOthQyog8c0kqDDRx5XyWa3hNTlQf6dT2jiDWJ4oOzHNcJZYJ1r2b-lkaF0XeeQbjcXtczdbU_0O78xhnaQD1qn4pp1AQwTdVlfM-CbLIjNEspm5jXhCQnCZVXKc49NuzLK4NEcTFdcnKmqAbPydzjauN6iBs1NtSiuECLVL1lESQ4WxA1cjmFB6ljH_ElR_79mDRLS4VZ93OLHJrEG9PqNUPm6bqix1F9Jeug2gFnzsPGhzPSK1lhk="
+    STRING_SESSION = "your session"
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "5983600849:AAFJMUaeNx2S0dIqklo3HjNvermZNhUiLFM"
+    TG_BOT_TOKEN = "your token"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -1001762139427
+    PRIVATE_GROUP_BOT_API_ID =
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
-    VC_SESSION = "1BVtsOIQBu3mJL9Wx6W2eRMl-FqKWHbUNmFIqhuYYE-DkJLvN98dz9aCT_pu8Uqw5gBaXylsjIaRRdnFVVtXqsR6O4r0jZlhFkFId_EExVrRJuKJWGpjc5mstXGc7nG4CQNAUQrTP-nBGoDBKpRGVWeeR2GKHNIgRc6JaIWL0IfgbkyC21X4wjtH1p-6-ZWGtv7fll_z6maSrKjlfgAe1P94bigDjvNBcpip12GELD21Ft1AxQ3d_bvyHz_KqESD-ja9kpJd-W_9l3EwpPDmk68z8MsruHYf75yYEaDRaaAFYLuSnqLF0Y6ad9nW9EuG1jfrPBtYbH0FefyHOHcAFNgen0gqpoik="
+    VC_SESSION = "your 2nd id telethon session" #note don't use maim account to vc player
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
     EXTERNAL_REPO = "https://github.com/rishabhanand2/tha_plugins"
     #don't edit this 
-    THANOSABUSE = "False"#don't edit this
+    THANOSABUSE = "False"#don't edit this else error in 
